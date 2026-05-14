63adc28584fad1e8024c6ca81dabaa31e297d77a
Pintu Gupta <vickygup9900@gmail.com>
2026-05-14 07:33:18 +0530

    Project structure done

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creatinng directories for the project
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/analysis.ipynb
+touch requirements.txt
+
+
+
+echo "Project structure created successfully!"
